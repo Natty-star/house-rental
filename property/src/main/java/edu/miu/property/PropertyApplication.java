7dc418e2c90d755901fabc439bc95677e7986840
Merhatsidk <merhatsidk29@gmail.com>
2022-10-20 15:35:25 -0500

    initial commit

---
+++ property/src/main/java/edu/miu/property/PropertyApplication.java
@@ -0,0 +1,15 @@
+package edu.miu.property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class PropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyApplication.class, args);
+	}
+
+}
